456182e171aa0940aae90746dd4c0bd8a6a2342d
linjohnss <linjohn0903@gmail.com>
2022-08-19 03:37:53 +0800

    Use local variable to restore odom matrix

--- src/ndt_odometry.cpp
+++ src/ndt_odometry.cpp
@@ -23,7 +23,6 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr target_cloud(new pcl::PointCloud<pcl::PointX
 pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud(new pcl::PointCloud<pcl::PointXYZ>());
 pcl::PointCloud<pcl::PointXYZ>::Ptr transform_cloud(new pcl::PointCloud<pcl::PointXYZ>());
 ros::Subscriber sub_sourcecloud;
-ros::Subscriber sub_device_tf;
 ros::Publisher pub_targetcloud;
 ros::Publisher pub_transformcloud;
 pclomp::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ>::Ptr ndt_omp(new pclomp::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ>());
@@ -34,6 +33,8 @@ char map_frame[] = "start_of_service";
 char odom_frame[] = "ndt_transformed";
 char sub_cloud[] = "/tango/point_cloud";
 char lidar_map_dir[] = "/home/ros20/Desktop/ndt_ws/src/ndt_odometry/map/target_cloud.pcd";
+Eigen::Matrix4f pre_trans;
+Eigen::Matrix4f delta_trans;
 tf::StampedTransform transform_final;
 tf::StampedTransform transform;
 std::mutex mutex_;
@@ -48,14 +49,18 @@ int main(int argc, char** argv) {
     tf::TransformBroadcaster broadcaster;
     tf::TransformListener listener;
     create_map(lidar_map_dir);
-    ros::Rate rate(10.0);
+    ros::Rate rate(20.0);
     while (nh.ok()){
         if(is_initial) {
             try{
                 const std::lock_guard<std::mutex> lock(mutex_);
                 listener.waitForTransform(map_frame, "/camera_depth", ros::Time(0), ros::Duration(1.0) );
-                listener.lookupTransform(map_frame, "/camera_depth", ros::Time(0), transform_final);
-                broadcaster.sendTransform(tf::StampedTransform(transform_final, ros::Time::now(), map_frame, odom_frame));
+                listener.lookupTransform(map_frame, "/camera_depth", ros::Time(0), transform_final);                
+                Eigen::Quaterniond init_rotation_Qua = Eigen::Quaterniond(transform_final.getRotation());
+                Eigen::Translation3f init_translation (transform_final.getOrigin().x(), transform_final.getOrigin().y(), transform_final.getOrigin().z());
+                Eigen::Matrix3f init_rotation = init_rotation_Qua.toRotationMatrix().cast <float> ();
+                pre_trans = (init_translation * init_rotation).matrix ();
+                delta_trans = Eigen::Matrix4f::Identity();
             } catch (tf::TransformException ex){
                 ROS_WARN("%s",ex.what());
                 ros::Duration(1.0).sleep();
@@ -96,39 +101,30 @@ void cloud_callback(const sensor_msgs::PointCloud2::ConstPtr& msg) {
         voxelgrid.filter(*downsampled);
         source_cloud = downsampled;
         
-        //Set initial alignment estimate found using robot odometry.
-        try{
-            listener.waitForTransform(map_frame, odom_frame, ros::Time(0), ros::Duration(1.0) );
-            listener.lookupTransform(map_frame, odom_frame, ros::Time(0), transform);
-        } catch (tf::TransformException ex){
-            ROS_WARN("%s",ex.what());
-            ros::Duration(1.0).sleep();
-            return;
-        }
-        Eigen::Quaterniond init_rotation_Qua = Eigen::Quaterniond(transform.getRotation());
-        Eigen::Translation3f init_translation (transform.getOrigin().x(), transform.getOrigin().y(), transform.getOrigin().z());
-        Eigen::Matrix3f init_rotation = init_rotation_Qua.toRotationMatrix().cast <float> ();
-        Eigen::Matrix4f init_guess = (init_translation * init_rotation).matrix ();
+        //Set initial alignment estimate found using linear predicted pose.
+        Eigen::Matrix4f initial_pose_matrix;
+        initial_pose_matrix = pre_trans * delta_trans;
 
         ndt_omp->setInputSource(source_cloud);	
         ndt_omp->setTransformationEpsilon (0.001);
-        ndt_omp->setStepSize (0.1); 
+        ndt_omp->setStepSize (0.001); 
         ndt_omp->setResolution(1.0);
         ndt_omp->setMaximumIterations (100);
-        ndt_omp->setNumThreads(8);
-        ndt_omp->setNeighborhoodSearchMethod(pclomp::DIRECT1);
-        ndt_omp->align(*transform_cloud, init_guess);
-        std::cout << " score: " << ndt_omp->getFitnessScore() << std::endl;
-
-        Eigen::Affine3d target_pose(ndt_omp->getFinalTransformation().cast <double> ());
-        if((fabs(target_pose(0,3))+fabs(target_pose(1,3))+fabs(target_pose(2,3))) > 0.4)        //transform pose
-        {
+        ndt_omp->setNumThreads(10);
+        ndt_omp->setNeighborhoodSearchMethod(pclomp::DIRECT7);
+        ndt_omp->align(*transform_cloud, initial_pose_matrix);
+        ROS_INFO(" score: %f", ndt_omp->getFitnessScore());
+        Eigen::Affine3f target_pose(ndt_omp->getFinalTransformation());
+        if((fabs(target_pose(0,3))+fabs(target_pose(1,3))+fabs(target_pose(2,3))) > 0.2) {
+            ROS_INFO(" transformed!");
             const std::lock_guard<std::mutex> lock(mutex_);
-            tf::transformEigenToTF(target_pose, transform_final);
+            tf::transformEigenToTF(target_pose.cast <double> (), transform_final);
             pub_transformcloud.publish( pcl2pointcloud(transform_cloud, map_frame));
+            delta_trans = pre_trans.inverse() * target_pose.matrix();
+            pre_trans = target_pose.matrix();
         }
     }
-    pub_targetcloud.publish( pcl2pointcloud(target_cloud, map_frame));
+    // pub_targetcloud.publish( pcl2pointcloud(target_cloud, map_frame));
     return;
 }
 
@@ -143,11 +139,12 @@ void create_map(char *map_dir)
     voxelgrid.setLeafSize(0.05f, 0.05f, 0.05f);
     voxelgrid.setInputCloud(target_cloud);
     voxelgrid.filter(*downsampled);
+    // lidar frame transform to camera frame
     Eigen::Affine3f r = Eigen::Affine3f::Identity();
     r.translation() << 0.0, 0.0, 0.0;
     r.rotate(Eigen::AngleAxisf(M_PI/2, Eigen::Vector3f::UnitZ()));
     pcl::transformPointCloud(*downsampled, *target_cloud, r);
-
-    std::cout << "Loaded " << target_cloud->size () << " data points from target" << std::endl;
+    ROS_INFO("Loaded %d data points from target", target_cloud->size ());
     ndt_omp->setInputTarget(target_cloud);
+    pub_targetcloud.publish( pcl2pointcloud(target_cloud, map_frame));
 }
--- src/ndt_optimizer.cpp
+++ src/ndt_optimizer.cpp
@@ -17,140 +17,122 @@
 
 sensor_msgs::PointCloud2 pcl2pointcloud (pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, char *fix_frame);
 void cloud_callback(const sensor_msgs::PointCloud2::ConstPtr& msg);
+void create_map(char *map_dir);
+
 pcl::PointCloud<pcl::PointXYZ>::Ptr target_cloud(new pcl::PointCloud<pcl::PointXYZ>());
 pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud(new pcl::PointCloud<pcl::PointXYZ>());
 pcl::PointCloud<pcl::PointXYZ>::Ptr transform_cloud(new pcl::PointCloud<pcl::PointXYZ>());
-bool TransformToMatrix(const tf::StampedTransform& stemp_transform, Eigen::Matrix4f& transform_matrix);
-void create_map(void);
 ros::Subscriber sub_sourcecloud;
 ros::Subscriber sub_device_tf;
 ros::Publisher pub_targetcloud;
 ros::Publisher pub_transformcloud;
+pclomp::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ>::Ptr 
+    ndt_omp(new pclomp::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ>());
+
 
 bool is_initial;
 char map_frame[] = "map";
 char odom_frame[] = "start_of_service";
 char sensor_frame[] = "camera_depth";
 char sub_cloud[] = "/tango/point_cloud";
+char lidar_map_dir[] = "/home/ros20/Desktop/ndt_ws/src/ndt_odometry/map/target_cloud.pcd";
 tf::StampedTransform transform_final;
 tf::StampedTransform transform;
 std::mutex mutex_;
 
-int main(int argc, char** argv) 
-{
+int main(int argc, char** argv) {
     ros::init (argc, argv, "ndt_odometry");
     ros::NodeHandle nh;
     sub_sourcecloud = nh.subscribe (sub_cloud, 1, cloud_callback);
-    pub_targetcloud = nh.advertise<sensor_msgs::PointCloud2> ("/target_cloud", 1);
-    pub_transformcloud = nh.advertise<sensor_msgs::PointCloud2> ("/transform_cloud", 1);
+    pub_targetcloud = nh.advertise<sensor_msgs::PointCloud2> ("/target_cloud", 10);
+    pub_transformcloud = nh.advertise<sensor_msgs::PointCloud2> ("/transform_cloud", 10);
     is_initial = true;
-    // ros::spin ();
     tf::TransformBroadcaster broadcaster;
     tf::TransformListener listener;
-    transform_final.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
-    tf::Quaternion q;
-    q.setRPY(0, 0, -M_PI/2);
-    transform_final.setRotation(q);
-    create_map();
-    ros::Rate rate(30.0);
+    create_map(lidar_map_dir);
+    ros::Rate rate(10.0);
     while (nh.ok()){
-        // const std::lock_guard<std::mutex> lock(mutex_);
-        broadcaster.sendTransform(tf::StampedTransform(transform_final, ros::Time::now(), map_frame, odom_frame));
+        if(is_initial) {
+            // identity
+            const std::lock_guard<std::mutex> lock(mutex_);
+            transform_final.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
+            tf::Quaternion q;
+            q.setRPY(0, 0, 0);
+            transform_final.setRotation(q);
+            is_initial = false;
+        }
+        else {
+            const std::lock_guard<std::mutex> lock(mutex_);
+            broadcaster.sendTransform(tf::StampedTransform(transform_final, ros::Time::now(), map_frame, odom_frame));
+        }
         rate.sleep();
         ros::spinOnce();
     }
-    
 	return 0;
 }
 
-sensor_msgs::PointCloud2 pcl2pointcloud (pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, char *fix_frame) 
-{
+sensor_msgs::PointCloud2 pcl2pointcloud (pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, char *fix_frame) {
     sensor_msgs::PointCloud2 ros_cloud;
     pcl::toROSMsg(*cloud, ros_cloud); //convert from PCL to ROS type this way
     ros_cloud.header.frame_id = fix_frame;
     return ros_cloud;
 }
 
-void cloud_callback(const sensor_msgs::PointCloud2::ConstPtr& msg) 
-{
+void cloud_callback(const sensor_msgs::PointCloud2::ConstPtr& msg) {
     //tf parameter
-    // static tf::Transform transform_pose;
-    //initialize
-    if(is_initial) {
-        // if(pcl::io::loadPCDFile("/home/ros20/Desktop/ndt_ws/src/ndt_odometry/map/target_cloud.pcd", *target_cloud)) {
-        //     std::cerr << "failed to load " << std::endl;
-        //     return;
-        // }
-        // pcl::PointCloud<pcl::PointXYZ>::Ptr downsampled(new pcl::PointCloud<pcl::PointXYZ>());
-        // pcl::VoxelGrid<pcl::PointXYZ> voxelgrid;
-        // voxelgrid.setLeafSize(0.05f, 0.05f, 0.05f);
-        // voxelgrid.setInputCloud(target_cloud);
-        // voxelgrid.filter(*downsampled);
-        // target_cloud = downsampled;
-
-    	// std::cout << "Loaded " << target_cloud->size () << " data points from target" << std::endl;
-        // is_initial = false;
-        // transform.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
-        // tf::Quaternion q;
-        // q.setRPY(0, 0, -M_PI/2);
-        // transform.setRotation(q);
-        // broadcaster.sendTransform(tf::StampedTransform(transform, ros::Time::now(), map_frame, odom_frame));
-
-    }
-    else{
-        static tf::TransformBroadcaster broadcaster;
-        static tf::TransformListener listener;
+    static tf::TransformListener listener;
+    static tf::StampedTransform transform;
+    // initialize
+    if(!is_initial) {
         pcl::fromROSMsg (*msg, *source_cloud);
     
         //downsampling
         pcl::PointCloud<pcl::PointXYZ>::Ptr downsampled(new pcl::PointCloud<pcl::PointXYZ>());
         pcl::VoxelGrid<pcl::PointXYZ> voxelgrid;
-        voxelgrid.setLeafSize(0.1f, 0.1f, 0.1f);
+        voxelgrid.setLeafSize(0.05f, 0.05f, 0.05f);
         voxelgrid.setInputCloud(source_cloud);
         voxelgrid.filter(*downsampled);
         source_cloud = downsampled;
         
         //Set initial alignment estimate found using robot odometry.
-        listener.waitForTransform(odom_frame, sensor_frame, ros::Time(0), ros::Duration(1.0));
-        listener.lookupTransform(odom_frame, sensor_frame, ros::Time(0), transform);
-        // Eigen::Quaterniond init_rotation_Qua = Eigen::Quaterniond(transform.getRotation());
-        // Eigen::Translation3f init_translation (transform.getOrigin().x(), transform.getOrigin().y(), transform.getOrigin().z());
-        // Eigen::Matrix3f init_rotation = init_rotation_Qua.toRotationMatrix().cast <float> ();
-        // Eigen::Matrix4f init_guess = (init_translation * init_rotation).matrix ();
-        Eigen::Matrix4f init_guess, base_frame;
-        TransformToMatrix(transform, init_guess);
-        TransformToMatrix(transform_final, base_frame);
-        init_guess = init_guess * base_frame;
-        pclomp::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ>::Ptr ndt_omp(new pclomp::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ>());
-        ndt_omp->setInputTarget(target_cloud);
+        try{
+            listener.waitForTransform(map_frame, sensor_frame, ros::Time(0), ros::Duration(1.0) );
+            listener.lookupTransform(map_frame, sensor_frame, ros::Time(0), transform);
+        } catch (tf::TransformException ex){
+            ROS_WARN("%s",ex.what());
+            ros::Duration(1.0).sleep();
+            return;
+        }
+        Eigen::Quaterniond init_rotation_Qua = Eigen::Quaterniond(transform.getRotation());
+        Eigen::Translation3f init_translation (transform.getOrigin().x(), transform.getOrigin().y(), transform.getOrigin().z());
+        Eigen::Matrix3f init_rotation = init_rotation_Qua.toRotationMatrix().cast <float> ();
+        Eigen::Matrix4f init_guess = (init_translation * init_rotation).matrix ();
+
         ndt_omp->setInputSource(source_cloud);	
-        ndt_omp->setTransformationEpsilon (0.01);
-        ndt_omp->setStepSize (0.1); 
+        ndt_omp->setTransformationEpsilon (0.001);
+        ndt_omp->setStepSize (0.05); 
         ndt_omp->setResolution(1.0);
         ndt_omp->setMaximumIterations (100);
-        ndt_omp->setNumThreads(6);
+        ndt_omp->setNumThreads(8);
         ndt_omp->setNeighborhoodSearchMethod(pclomp::DIRECT7);
         ndt_omp->align(*transform_cloud, init_guess);
         std::cout << " score: " << ndt_omp->getFitnessScore() << std::endl;
 
         Eigen::Affine3d target_pose(ndt_omp->getFinalTransformation().cast <double> ());
-        if((fabs(target_pose(0,3))+fabs(target_pose(1,3))+fabs(target_pose(2,3))) > 0.4)        //transform pose
-        {
-            target_pose.matrix() = target_pose.matrix() * init_guess.inverse().cast <double> ();
+        if((fabs(target_pose(0,3))+fabs(target_pose(1,3))+fabs(target_pose(2,3))) > 0.4) {
+            target_pose.matrix() = init_guess.inverse().cast <double> () * target_pose.matrix();
             const std::lock_guard<std::mutex> lock(mutex_);
             tf::transformEigenToTF(target_pose, transform_final);
-            // broadcaster.sendTransform(tf::StampedTransform(transform_pose, ros::Time::now(), map_frame, odom_frame));
             pub_transformcloud.publish( pcl2pointcloud(transform_cloud, map_frame));
         }
-
     }
-    pub_targetcloud.publish(pcl2pointcloud(target_cloud, map_frame));
+    pub_targetcloud.publish( pcl2pointcloud(target_cloud, map_frame));
     return;
 }
 
-void create_map(void)
+void create_map(char *map_dir)
 {
-    if(pcl::io::loadPCDFile("/home/ros20/Desktop/ndt_ws/src/ndt_odometry/map/target_cloud.pcd", *target_cloud)) {
+    if(pcl::io::loadPCDFile(map_dir, *target_cloud)) {
         std::cerr << "failed to load " << std::endl;
         return;
     }
@@ -159,23 +141,13 @@ void create_map(void)
     voxelgrid.setLeafSize(0.05f, 0.05f, 0.05f);
     voxelgrid.setInputCloud(target_cloud);
     voxelgrid.filter(*downsampled);
-    target_cloud = downsampled;
+    // lidar frame transform to camera frame
+    Eigen::Affine3f r = Eigen::Affine3f::Identity();
+    r.translation() << 0.0, 0.0, 0.0;
+    r.rotate(Eigen::AngleAxisf(M_PI/2, Eigen::Vector3f::UnitZ()));
+    pcl::transformPointCloud(*downsampled, *target_cloud, r);
+    // target_cloud = downsampled;
 
     std::cout << "Loaded " << target_cloud->size () << " data points from target" << std::endl;
-    is_initial = false;
-}
-
-bool TransformToMatrix(const tf::StampedTransform& stemp_transform, Eigen::Matrix4f& transform_matrix) 
-{
-    Eigen::Translation3f tl_btol(
-    stemp_transform.getOrigin().getX(), 
-    stemp_transform.getOrigin().getY(), 
-    stemp_transform.getOrigin().getZ());
-    double roll, pitch, yaw;
-    tf::Matrix3x3(stemp_transform.getRotation()).getEulerYPR(yaw, pitch, roll);
-    Eigen::AngleAxisf rot_x_btol(roll, Eigen::Vector3f::UnitX());
-    Eigen::AngleAxisf rot_y_btol(pitch, Eigen::Vector3f::UnitY());
-    Eigen::AngleAxisf rot_z_btol(yaw, Eigen::Vector3f::UnitZ());
-    transform_matrix = (tl_btol * rot_z_btol * rot_y_btol * rot_x_btol).matrix();
-    return true;
+    ndt_omp->setInputTarget(target_cloud);
 }
